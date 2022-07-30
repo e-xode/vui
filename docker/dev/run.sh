eac47c31a8c53d3589934aeaf51e8e7899314d90
Christophe Bragard <christophe.bragard@e-xode.net>
2022-07-30 20:46:23 +0200

    [master] changes envs dev / prod

--- docker/dev/run.sh
+++ docker/dev/run.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-if [ "$NODE_ENV" = "development" ]
-then
 npm run dev
-else
-npm start
-fi
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker run -d --mount type=bind,source="$(pwd)/logs",target=/app/logs --network e-xode --name e-xode.vui -p 3001:3001 krisae/e-xode.vui:latest
+docker run -d \
+--restart=unless-stopped \
+--mount type=bind,source="$(pwd)/logs",target=/app/logs \
+--network e-xode --name e-xode.vui -p 127.0.0.1:3001:3001 krisae/e-xode.vui:latest
