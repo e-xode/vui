f563bbb3fc8e75d149bc9199838b3341def0d218
Christophe Bragard <christophe.bragard@e-xode.net>
2023-01-13 19:40:35 +0100

    [development] grid-unit flex (#38)

--- src/doc/views/component/layout/grid/unit/unit.vue
+++ src/doc/views/component/layout/grid/unit/unit.vue
@@ -5,7 +5,35 @@
             col-md="2"
         >
             <vui-grid-unit class="examples">
-
+                <template
+                    v-for="example in examples"
+                    :key="`example--${example.gridid}`"
+                >
+                    <vui-grid
+                        :id="example.gridid"
+                        col-sm="1"
+                        col-md="2"
+                    >
+                        <vui-grid-unit
+                            col="30"
+                        >
+                            <p>
+                                {{ $t('page.component.grid-unit.left') }}
+                            </p>
+                        </vui-grid-unit>
+                        <vui-grid-unit
+                            col="70"
+                        >
+                            <p>
+                                {{ $t('page.component.grid-unit.right') }}
+                            </p>
+                        </vui-grid-unit>
+                    </vui-grid>
+                    <div
+                        class="highlighted-code"
+                        v-html="example.highlighted"
+                    />
+                </template>
             </vui-grid-unit>
             <vui-grid-unit class="api-options">
                 <vui-card>
