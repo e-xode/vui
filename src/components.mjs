be77a2139d6c58fc9312beea4866fa9baf017489
christophe bragard <christophe.bragard@e-xode.net>
2023-01-15 20:42:58 +0100

    [development] fix production import path

--- src/components.mjs
+++ src/components.mjs
@@ -10,11 +10,14 @@ export default {
             import: 'default'
         })
         for (const path in modules) {
-            const { name } = jsonComponents.find((c) => c.path === path)
-            app.component(
-                name,
-                defineAsyncComponent(modules[path])
-            )
+            const component = jsonComponents.find((c) => c.path === path)
+            console.log(component)
+            if (component) {
+                app.component(
+                    component.name,
+                    defineAsyncComponent(modules[path])
+                )
+            }
         }
     }
 }
