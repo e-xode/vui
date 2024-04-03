9ea806d0ee8776e182e53c05f0624fcfd69553fe
Christophe BRAGARD <christophe.bragard@e-xode.net>
2024-04-03 21:12:20 +0200

    [development] fix components path

--- src/components.mjs
+++ src/components.mjs
@@ -6,11 +6,11 @@ import jsonComponents from '@/components.json'
 export default {
     install(app) {
         app.provide('$bus', mitt())
-        const modules = import.meta.glob('@/components/**/*.vue', {
-            import: 'default'
-        })
+        const modules = import.meta.glob('@/components/**/*.vue')
         for (const _path in modules) {
-            const path = _path.substring(2)
+            const path = _path.startsWith('..')
+                ? _path.slice(2)
+                : _path
             const component = jsonComponents.find((c) => c.path === path)
             if (component) {
                 app.component(
